7825b04d62ddb7bc863d683082b341b2ffcf1faa
Kendall Umaña <kendaltuanis@hotmail.com>
2017-07-14 15:29:51 -0600

    Added Views: (Downloadsand Addons)

---
+++ WowLatinos/Controllers/Downloads.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WowLatinos.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace WowLatinos.Controllers
+{
+    public class Downloads : Controller
+    {
+        public IActionResult Game()
+        {
+            return View();
+        }
+
+        public IActionResult Addons()
+        {
+            return View();
+        }
+
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
